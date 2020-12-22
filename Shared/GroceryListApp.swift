e8d2b54572ec0b8aed848fdfd6702b3ff1ebc67d
slichter <lichter.sarahe@gmail.com>
2020-12-22 11:55:50 -0600

    initial commit

---
+++ Shared/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  Shared
+//
+//  Created by Sarah Lichter on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
