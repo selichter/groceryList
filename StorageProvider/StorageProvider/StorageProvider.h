51099069e354484142b0f6f5a50d99fc4b124189
selichter <lichter.sarahe@gmail.com>
2021-01-12 13:52:11 -0600

    added storage provider framework to workspace

---
+++ StorageProvider/StorageProvider/StorageProvider.h
@@ -0,0 +1,18 @@
+//
+//  StorageProvider.h
+//  StorageProvider
+//
+//  Created by Sarah Lichter on 1/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageProvider.
+FOUNDATION_EXPORT double StorageProviderVersionNumber;
+
+//! Project version string for StorageProvider.
+FOUNDATION_EXPORT const unsigned char StorageProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageProvider/PublicHeader.h>
+
+
